4741c42a5f29d45f66ea92935795d0ee3faea2de
mare128 <ball_marius@web.de>
2020-06-07 12:04:10 +0200

    Final2

---
+++ Ball_Marius_Uebung_06/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator animator;
+    RaycastHit ray;
+    Vector3 destination;
+    bool isMoving;
+    int waitFor = 2;
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        destination = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isMoving = false;
+            animator.ResetTrigger("run");
+            agent.SetDestination(transform.position);
+            animator.SetTrigger("attack");
+            UnityEngine.Debug.Log("attack");
+        }
+        else// if (isMoving && waitFor > 0) {
+            //  waitFor--;
+            //}else 
+        if (transform.position.x == destination.x && transform.position.z == destination.z)
+        {
+            isMoving = false;
+            animator.SetTrigger("idle");
+            UnityEngine.Debug.Log("idle");
+        }
+        else {
+            if(isMoving)
+            animator.SetTrigger("run");
+        }
+        
+    }
+
+    public void Move(RaycastHit raycast) {
+        isMoving = true;
+        animator.SetTrigger("run");
+        animator.SetTrigger("run");
+        UnityEngine.Debug.Log("start");
+        destination = raycast.point;
+        agent.SetDestination(raycast.point);
+        UnityEngine.Debug.Log("end");
+    }
+}
